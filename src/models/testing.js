957954d01971ee105c8dfe58c76d90168838081a
itz-XD <152062675+itz-XD@users.noreply.github.com>
2025-05-29 20:00:00 +0530

    add console 5 times

--- src/models/testing.js
+++ src/models/testing.js
@@ -1,3 +1,9 @@
 console.log('Testing model loaded');
 console.log('Testing model loaded');
 console.log('Testing model loaded');
+console.log('Testing model loaded');
+console.log('Testing model loaded');
+console.log('Testing model loaded');
+console.log('Testing model loaded');
+console.log('Testing model loaded');
+console.log('Testing model loaded');
